009cb76c38e91c934a49fd3ce18e793c7b7d26e1
gilbert <grrr104@hotmail.com>
2019-02-25 15:45:08 -0600

    added comments made methods a bit more readable and changed it so if a valid password was inputed all values reset upon a new input

--- pw.py
+++ pw.py
@@ -27,12 +27,9 @@ class Passwords:
                 self._passList = pickle.load(handle)
         else:
             with open("newFile.pickle", "wb") as handle:
-                pickle.dump(self._passList,handle,protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(self._passList, handle, protocol=pickle.HIGHEST_PROTOCOL)
             with open("newFile.pickle", 'rb') as handle:
                 self.passList = pickle.load(handle)
-
-    def _getPasswordLength(self):
-        return len(self._password)
 
     def _setPassword(self, p):
         self._password = p
@@ -40,21 +37,41 @@ class Passwords:
     def _setKey(self, k):
         self._key = k
 
-    def _generatePassword(self):
-        randPas = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for n in range(12)])
+    """
+        Generates a password depending whether or not a valid length of was provided. If no valid length it goes to 
+        default value of 12. Sets the random password to be a combination of letters(cap and lower digits and symbols
+    """
+    def _generatePassword(self, v):
+        if v.isdigit():
+            pasLength = int(v)
+        else:
+            pasLength = 6
+
+        randPas = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation)
+                           for n in range(pasLength)])
         self._setPassword(randPas)
 
-    def _addKeyValue(self):
-        self._passList.update({str(self._key):str(self._password)})
-        with open('newFile.pickle','wb') as handle:
-            pickle.dump(self._passList,handle,protocol=pickle.HIGHEST_PROTOCOL)
+    """
+        Adds the key-pair value to the dictionary [_passList] and updates the .pickle file with the new dictionary info
+    """
+    def _saveKeyPairValue(self):
+        self._passList.update({str(self._key): str(self._password)})
+        with open('newFile.pickle', 'wb') as handle:
+            pickle.dump(self._passList, handle, protocol=pickle.HIGHEST_PROTOCOL)
         input('Press enter to continue...')
 
+    """
+        Searches the dictionary to check if the key of the class exists within it
+    """
     def _searchForKey(self):
         if self._key in self._passList:
             return True
         return False
 
+    """
+        Searches the key given by the user. If found sets both the key and password to the class in order to retrive 
+        the information to the user
+    """
     def _searchForExistingKey(self, k):
         print('searching')
         if k in self._passList:
@@ -63,25 +80,31 @@ class Passwords:
             return True
         return False
 
+    """
+        Checks the length of the password for a valid length set to 5 or greater as default
+    """
     def _checkIfValidLength(self):
-        pLen = self._getPasswordLength()
-        if pLen > 7:
+        pLen = len(self._password)
+        if pLen > 5:
             self._minLength = True
         else:
-            input('Password not a long enough!!')
-            
-    def _checkifValidInput(self):
+            print('Invalid password length')
+            input('Press any button to cont...')
 
-        """ print('\n\nmyPass: ', myPass)
-        print('myPass Length: ', self._getPasswordLength())
-        print('position: ', position) """
+    """
+        Validates user inputed password; ticks off flags to make sure password is correct
+    """
+    def _checkifValidInput(self):
         position = 0
-        while position < self._getPasswordLength():
+        while position < len(self._password):
             if str(self._password[position]).isspace():
                 self._noSpace = False
+                print('Space in password. Invalid')
+                input('Press any button to cont...')
                 break
             if str(self._password[position]).isdigit():
                 self._hasNumber = True
+                print('digit pLen[i]:', self._password[position])
                 position += 1
 
             elif str(self._password[position]).isalpha():
@@ -98,38 +121,59 @@ class Passwords:
         if not self._weirdSymbol:
             input('No weird Symbol in password!!')
 
+    """
+        Gets the key and password from user and checks if its a user inputed password or to generate a password
+        calls the appropriate method
+    """
     def _addInformation(self, key, pas):
         self._setKey(key)
-        if pas == '!gen':
-            self._generatePassword()
+        if '!gen' in pas:
+            val = pas.split(' ')
+            if len(val) > 1:
+                print(val[1])
+                self._generatePassword(val[1])
+
+            else:
+                self._generatePassword(val[0])
         else:
             self._setPassword(pas)
 
+        # if the list is empty calls check validation for password
         if not bool(self._passList):
-            self._checkValidation()
+            self._checkPasswordValidation()
             print('was empty adding items')
         else:
-            self._addKeyPair()
+            self._checkKeyPairValue()
 
-    def _checkValidation(self):
+    """
+        Checks password. Calls to see if length and valid inputs. If corrects adds 
+    """
+    def _checkPasswordValidation(self):
         self._checkIfValidLength()
         self._checkifValidInput()
-        if self._minLength and self._weirdSymbol and self._capLetter:
+        if self._minLength and self._weirdSymbol and self._capLetter and self._noSpace:
             input('updated new key-value')
-            self._addKeyValue()
+            self._saveKeyPairValue()
 
-    def _addKeyPair(self):
+    """
+        Checks if there is an existing key that the user inputed if yes, asks if user wants to replace password else do
+        nothing. Also checks for password validation if all correct saves to .pickle file
+    """
+    def _checkKeyPairValue(self):
         foundKey = self._searchForKey()
         if not foundKey:
             print('No key found')
-            self._checkValidation()
+            self._checkPasswordValidation()
         else:
             changeKey = input('Do you want to update existing key?')
             if changeKey == 'y':
-                self._checkValidation()
+                self._checkPasswordValidation()
             else:
-                input('req not met!')
+                input('Press any button cont...')
 
+    """
+        Gets the password from user inputed key if key exists
+    """
     def _getPassFromDict(self, k):
         if self._searchForExistingKey(k):
             print('Key Exists')
@@ -139,17 +183,27 @@ class Passwords:
         else:
             print('No account in database')
 
+    """
+        prints dictionary items
+    """
     def printDictionary(self):
         for k, v in self._passList.items():
             print('key: ' + k)
             print('value: ' + v)
         input('Current Items. \nPress enter to continue...')
 
-    def showLocation(self):
+    def _resetParameters(self):
+        self._noSpace = True
+        self._minLength = False
+        self._capLetter = False
+        self._weirdSymbol = False
+        self._hasLetter = False
+        self._hasNumber = False
+    """def showLocation(self):
         return self._key
 
     def showPassword(self):
-        return self._password
+        return self._password"""
 
 
 def switchInput(inputValue):
@@ -168,6 +222,9 @@ def main():
         print('Usage: python pw.py[account] - copy account password')
         app_on = True
         while app_on:
+            # rests all parameters of the password
+            p._resetParameters()
+
             os.system('cls' if os.name == 'nt' else 'clear')
             takeMeTo = input('\'!display\', \'input\', \'!q\'\nWhat do you want to do? ')
             iv = switchInput(takeMeTo)
@@ -186,9 +243,10 @@ def main():
                 print('invalid input')
         sys.exit()
     else:
-        account = sys.argv[1] #first command line arg is the account name
+        account = sys.argv[1]  # first command line arg is the account name
         print('Account: {}'.format(account))
         p._getPassFromDict(account)
+
 
 if __name__ == "__main__":
     main()
